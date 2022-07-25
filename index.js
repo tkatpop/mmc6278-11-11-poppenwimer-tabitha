e895265aeed75e124dc37319db6bad72ac434d49
tkatpop <93983050+tkatpop@users.noreply.github.com>
2022-07-25 16:49:28 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const getPoemBtn = document.getElementById('get-poem')
+const poemEl = document.getElementById('poem')
+const poemURL = 'https://poetrydb.org/random,linecount/1;12/author,title,lines.json'
+
+const getJSON = url => fetch(url).then(res => res.json())
+
+const pipe = (...fns) => firstArg => fns.reduce((returnValue, fn) => fn(returnValue), firstArg)
+
+const makeTag = tag => str => `<${tag}>${str}</${tag}>`
+
+// complete this function
+const makePoemHTML = () => {}
+
+// attach a click event to #get-poem
+getPoemBtn.onclick = async function() {
+  // renders the HTML string returned by makePoemHTML to #poem
+  poemEl.innerHTML = makePoemHTML(await getJSON(poemURL))
+}
